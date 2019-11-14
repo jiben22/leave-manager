47307683efa66a895eacba5994e788df27dfd39a
Corentin <bosquet.corentin@gmail.com>
2019-11-14 18:46:13 +0100

    Update datetimepicker for leaveRequest

--- src/main/resources/static/js/locales/bootstrap-datetimepicker.fr.js
+++ src/main/resources/static/js/locales/bootstrap-datetimepicker.fr.js
@@ -1,12 +1,31 @@
-;(function($){
-    $.fn.datetimepicker.dates['fr'] = {
-        days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
-        daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
-        daysMin: ["D", "L", "Ma", "Me", "J", "V", "S", "D"],
-        months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
-        monthsShort: ["Jan", "Fev", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"],
-        today: "Aujourd'hui",
-        weekStart: 1,
-        format: "dd/mm/yyyy"
-    };
-}(jQuery));
+$(function () {
+
+    var d = new Date();
+
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+
+    var output = (day<10 ? '0' : '') + day + '/' + (month<10 ? '0' : '') + month + '/' + d.getFullYear()
+
+    $(".datepicker").val(output + " 00:00");
+    $('.datepicker').datetimepicker({
+        locale:'fr',
+        format:'DD/MM/YYYY HH:00',
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            /*up: "fa fa-chevron-up",
+            down: "fa fa-chevron-down",*/
+            previous: "fa fa-chevron-left",
+            next: "fa fa-chevron-right",
+            today: "fa fa-screenshot",
+            clear: "fa fa-trash",
+            close: "fa fa-remove"
+        },
+        widgetPositioning:{
+            horizontal: 'auto',
+            vertical: 'bottom'
+        },
+        enabledHours: [0, 12]
+    });
+});
