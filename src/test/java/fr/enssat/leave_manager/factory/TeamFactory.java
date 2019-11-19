91166bfcde6966139f9e3f93ebac2f58e1847541
Jiben <jibenapps@gmail.com>
2019-11-19 17:36:32 +0100

    Create tests for TeamServiceImpl

--- src/test/java/fr/enssat/leave_manager/factory/TeamFactory.java
+++ src/test/java/fr/enssat/leave_manager/factory/TeamFactory.java
@@ -4,6 +4,7 @@ import fr.enssat.leave_manager.model.EmployeeEntity;
 import fr.enssat.leave_manager.model.TeamEntity;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class TeamFactory {
@@ -19,5 +20,34 @@ public class TeamFactory {
                 .department(DepartmentFactory.getDepartment1().get())
                 .employeeList(employees)
                 .build();
+    }
+
+    public static Optional<TeamEntity> getTeam2() {
+
+        Set<EmployeeEntity> employees = new HashSet<>();
+        employees.add(EmployeeFactory.getEmployee2().get());
+        // Missing an employee
+
+        return Optional.ofNullable(TeamEntity.builder()
+                .id("TEAM-157314099170606-0002")
+                .name("Bifrost traveler")
+                .teamLeader(TeamLeaderFactory.getTeamLeader2().get())
+                .department(DepartmentFactory.getDepartment1().get())
+                .employeeList(employees)
+                .build());
+    }
+
+    public static Optional<TeamEntity> getTeam3() {
+
+        Set<EmployeeEntity> employees = new HashSet<>();
+        employees.add(EmployeeFactory.getEmployee2().get());
+
+        return Optional.ofNullable(TeamEntity.builder()
+                .id("TEAM-157314099170606-0003")
+                .name("Frozen")
+                .teamLeader(TeamLeaderFactory.getTeamLeader2().get()) // Bad team leader
+                .department(DepartmentFactory.getDepartment1().get()) // Bad department
+                .employeeList(employees)
+                .build());
     }
 }
--- src/test/java/fr/enssat/leave_manager/service/impl/TeamServiceImplTest.java
+++ src/test/java/fr/enssat/leave_manager/service/impl/TeamServiceImplTest.java
@@ -2,78 +2,127 @@ package fr.enssat.leave_manager.service.impl;
 
 import fr.enssat.leave_manager.factory.TeamFactory;
 import fr.enssat.leave_manager.model.TeamEntity;
+import fr.enssat.leave_manager.repository.TeamRepository;
 import fr.enssat.leave_manager.service.exception.not_found.TeamNotFoundException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class TeamServiceImplTest {
-//
-//    @Mock
-//    private TeamServiceImpl teamService;
-//
+
+    @Mock
+    private TeamRepository repository;
+
+    @InjectMocks
+    private TeamServiceImpl teamService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void testGetTeam() {
-//        TeamEntity team = teamService.getTeam("TEAM-157314099170606-0001");
-//
-//        assertEquals(team.getId(), "TEAM-157314099170606-0001");
-//        assertEquals(team.getName(), "Stark corporation");
+
+        Optional<TeamEntity> team2 = TeamFactory.getTeam2();
+
+        when(repository.findById(team2.get().getId()))
+                .thenReturn(team2);
+
+        TeamEntity team = teamService.getTeam(team2.get().getId());
+
+        assertThat(team).isEqualToComparingFieldByField(team2.get());
     }
-//
-//    @Test(expected = TeamNotFoundException.class)
-//    public void testGetTeamException() {
-//        TeamEntity team = teamService.getTeam("UNKNOWN ID");
-//    }
-//
-//    @Test
-//    public void testGetTeamByName() {
-//        List<TeamEntity> team_list = teamService.getTeamByName("Stark corporation");
-//
-//        assertNotNull(team_list);
-//        assertNotEquals(team_list.size(), 0);
-//
-//        TeamEntity team = team_list.get(0);
-//
-//        assertEquals(team.getId(), "TEAM-157314099170606-0001");
-//        assertEquals(team.getName(), "Stark corporation");
-//    }
-//
-//    @Test
-//    public void testGetTeams() {
-//        List<TeamEntity> team_list = teamService.getTeams();
-//
-//        assertNotNull(team_list);
-//        assertNotEquals(team_list.size(), 0);
-//    }
-//
-//    @Test
-//    public void testAddTeams() {
-//        TeamEntity team = TeamFactory.getTeam();
-//        TeamEntity added_team = teamService.addTeam(team);
-//
-//        assertEquals(team, added_team);
-//    }
-//
-//    @Test
-//    public void testEditTeams() {
-//        TeamEntity team = teamService.getTeam("TEAM-157314099170606-0001");
-//        team.setName("New Name Corporation");
-//        TeamEntity edited_team = teamService.editTeam(team);
-//
-//        assertEquals(team.getId(), "TEAM-157314099170606-0001");
-//        assertEquals(team.getName(), "New Name Corporation");
-//    }
-//
-//    @Test
-//    public void testDeleteTeam() {
-//        teamService.deleteTeam("TEAM-157314099170606-0001");
-//
-//        assertFalse(teamService.exists("TEAM-157314099170606-0001"));
-//    }
+
+    @Test(expected = TeamNotFoundException.class)
+    public void testGetTeamException() {
+
+        when(repository.findById("Unknown ID"))
+                .thenThrow(TeamNotFoundException.class);
+
+        teamService.getTeam("Unknown ID");
+    }
+
+    @Test
+    public void testGetTeamByName() {
+
+        List<TeamEntity> teamList1 = new ArrayList<>();
+        teamList1.add(TeamFactory.getTeam2().get());
+        teamList1.add(TeamFactory.getTeam3().get());
+
+        when(repository.findByName("Bifrost traveler"))
+                .thenReturn(teamList1);
+
+        List<TeamEntity> teamList = teamService.getTeamByName("Bifrost traveler");
+
+        assertEquals(teamList.size(), teamList1.size());
+        assertThat(teamList.get(0)).isEqualToComparingFieldByField(teamList1.get(0));
+        assertThat(teamList.get(1)).isEqualToComparingFieldByField(teamList1.get(1));
+    }
+
+    @Test
+    public void testGetTeams() {
+
+        List<TeamEntity> teamList1 = new ArrayList<>();
+        // Add teams with DESC sorting
+        teamList1.add(TeamFactory.getTeam3().get());
+        teamList1.add(TeamFactory.getTeam2().get());
+
+        when(repository.findAll(Sort.by(Sort.Direction.ASC, "name")))
+                .thenReturn(teamList1);
+
+        List<TeamEntity> teamList = teamService.getTeams();
+
+        assertEquals(teamList.size(), teamList1.size());
+        assertThat(teamList.get(0)).isEqualToComparingFieldByField(teamList1.get(0));
+        assertThat(teamList.get(1)).isEqualToComparingFieldByField(teamList1.get(1));
+    }
+
+    @Test
+    public void testAddTeam() {
+
+        TeamEntity team = TeamFactory.getTeam2().get();
+        when(repository.saveAndFlush(team))
+                .thenReturn(team);
+
+        TeamEntity addedTeam = teamService.addTeam(team);
+
+        assertThat(addedTeam).isEqualToComparingFieldByField(team);
+    }
+
+    @Test
+    public void testEditTeam() {
+
+        TeamEntity team = TeamFactory.getTeam2().get();
+        team.setName("Name");
+
+        when(repository.saveAndFlush(team))
+                .thenReturn(team);
+
+        TeamEntity editedTeam = teamService.editTeam(team);
+
+        assertThat(editedTeam).isEqualToComparingFieldByField(team);
+    }
+
+    @Test
+    public void testDeleteTeam() {
+
+        TeamEntity team = TeamFactory.getTeam2().get();
+        teamService.deleteTeam(team.getId());
+    }
 }
