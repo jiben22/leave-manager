3c9c57a5d61036b1d357a32caa5dc46e77b1ca4c
Jiben <jibenapps@gmail.com>
2019-11-23 18:26:10 +0100

    Configure Security

--- src/main/java/fr/enssat/leave_manager/config/SecurityConfig.java
+++ src/main/java/fr/enssat/leave_manager/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class MainSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
