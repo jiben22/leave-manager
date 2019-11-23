19bd8d4f9d5fd8d5cd5d9f6409d3b298cf70e6ce
Jiben <jibenapps@gmail.com>
2019-11-23 18:01:33 +0100

    Add employees to datatables

--- src/main/java/fr/enssat/leave_manager/controller/EmployeeController.java
+++ src/main/java/fr/enssat/leave_manager/controller/EmployeeController.java
@@ -1,16 +1,40 @@
 package fr.enssat.leave_manager.controller;
 
+import fr.enssat.leave_manager.model.EmployeeEntity;
+import fr.enssat.leave_manager.service.EmployeeService;
+import fr.enssat.leave_manager.service.impl.EmployeeServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class EmployeeController {
+
+    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeServiceImpl employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping("/employes")
     public String showEmployees(Model model) {
 
+        logger.debug("GET /employes");
+
         model.addAttribute("title", "Liste des employés");
+
+        // Get employees
+        List<EmployeeEntity> employees = employeeService.getEmployees();
+        model.addAttribute("employees", employees);
+
         return "employees";
     }
 
