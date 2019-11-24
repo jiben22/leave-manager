e72c3e6f84d60bda61ed91983168e30b25a5da24
Jiben <jibenapps@gmail.com>
2019-11-24 09:03:45 +0100

    Add controller for error page

---
+++ src/main/java/fr/enssat/leave_manager/controller/CustomErrorController.java
@@ -0,0 +1,33 @@
+package fr.enssat.leave_manager.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    Logger logger = LoggerFactory.getLogger(CustomErrorController.class);
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+
+        Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        logger.error("Error with status code " + statusCode + " happened");
+        model.addAttribute("statusCode", statusCode.toString());
+
+        return "error";
+    }
+}
