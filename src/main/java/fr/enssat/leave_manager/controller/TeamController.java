a39a0e85cb4a94e2d009b5a8b4414003c47897ba
Jiben <jibenapps@gmail.com>
2019-11-24 15:06:38 +0100

    Add Create / Read / Update form teams
    Create method not working (error due to TeamLeader: id)

--- src/main/java/fr/enssat/leave_manager/controller/TeamController.java
+++ src/main/java/fr/enssat/leave_manager/controller/TeamController.java
@@ -1,15 +1,30 @@
 package fr.enssat.leave_manager.controller;
 
+import fr.enssat.leave_manager.model.DepartmentEntity;
+import fr.enssat.leave_manager.model.EmployeeEntity;
 import fr.enssat.leave_manager.model.TeamEntity;
+import fr.enssat.leave_manager.model.TeamLeaderEntity;
+import fr.enssat.leave_manager.service.DepartmentService;
+import fr.enssat.leave_manager.service.EmployeeService;
+import fr.enssat.leave_manager.service.TeamLeaderService;
 import fr.enssat.leave_manager.service.TeamService;
+import fr.enssat.leave_manager.service.impl.DepartmentServiceImpl;
+import fr.enssat.leave_manager.service.impl.EmployeeServiceImpl;
+import fr.enssat.leave_manager.service.impl.TeamLeaderServiceImpl;
 import fr.enssat.leave_manager.service.impl.TeamServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -19,9 +34,19 @@ public class TeamController {
 
     private final TeamService teamService;
 
+    private final EmployeeService employeeService;
+
+    private final TeamLeaderService teamLeaderService;
+
+    private final DepartmentService departmentService;
+
     @Autowired
-    public TeamController(TeamServiceImpl teamService) {
+    public TeamController(TeamServiceImpl teamService, EmployeeServiceImpl employeeService,
+                          TeamLeaderServiceImpl teamLeaderService, DepartmentServiceImpl departmentService) {
         this.teamService = teamService;
+        this.employeeService = employeeService;
+        this.departmentService = departmentService;
+        this.teamLeaderService = teamLeaderService;
     }
 
     @GetMapping("/equipes")
@@ -35,21 +60,135 @@ public class TeamController {
         List<TeamEntity> teams = teamService.getTeams();
         model.addAttribute("teams", teams);
 
-
         return "teams";
     }
 
     @GetMapping("/equipe/ajouter")
-    public String showAddTeam(Model model) {
+    public String showAddTeamForm(Model model) {
+
+        logger.debug("GET /equipe/ajouter");
 
         model.addAttribute("title", "Ajouter une équipe");
-        return "addTeam";
+        model.addAttribute("team", new TeamEntity());
+
+        // Get employees
+        List<EmployeeEntity> employees =  employeeService.getEmployees();
+        model.addAttribute("employees", employees);
+
+        // Get departments
+        List<DepartmentEntity> departments = departmentService.getDepartments();
+        model.addAttribute("departments", departments);
+
+        return "addTeamForm";
     }
 
-    @GetMapping("/equipe/modifier")
-    public String showUpdateTeam(Model model) {
+    @GetMapping("/equipe/{id}")
+    public String showTeamById(@PathVariable String id, Model model) {
+
+        logger.debug("GET /equipe/" + id);
+
+        model.addAttribute("title", "Visualiser l'équipe");
+
+        // Get team by id
+        TeamEntity team = teamService.getTeam(id);
+        model.addAttribute("team", team);
+
+        return "showTeam";
+    }
+
+    @PostMapping("/equipe/ajouter")
+    public String submitAddTeamForm(@Valid @ModelAttribute("team") TeamEntity team,
+                                        BindingResult result, Model model,
+                                        RedirectAttributes redirectAttributes) {
+
+        logger.debug("POST /equipe/ajouter");
+
+        if (result.hasErrors()) {
+            logger.info(result.toString());
+
+            // Get employees
+            List<EmployeeEntity> employees =  employeeService.getEmployees();
+            model.addAttribute("employees", employees);
+
+            // Get departments
+            List<DepartmentEntity> departments = departmentService.getDepartments();
+            model.addAttribute("departments", departments);
+
+            // Return form with errors
+            return "addTeamForm";
+        } else {
+            try {
+                logger.error(team.getTeamLeader().toString());
+                team.setTeamLeader(team.getTeamLeader());
+                // Save team
+                team = teamService.addTeam(team);
+            } catch (Exception e) {
+                logger.error(e.getMessage() + e.getCause());
+                redirectAttributes.addFlashAttribute("message", "Impossible d'enregister l'équipe");
+
+                return "redirect:/equipes";
+            }
+        }
+
+        return "redirect:/equipe/" + team.getId();
+    }
+
+    @GetMapping("/equipe/modifier/{id}")
+    public String showUpdateTeamForm(@PathVariable String id, Model model) {
+
+        logger.debug("GET /equipe/modifier/" + id);
 
         model.addAttribute("title", "Modifier l'équipe");
-        return "updateTeam";
+
+        // Get team by id
+        TeamEntity team = teamService.getTeam(id);
+        model.addAttribute("team", team);
+
+        // Get employees
+        List<EmployeeEntity> employees =  employeeService.getEmployees();
+        model.addAttribute("employees", employees);
+
+        // Get departments
+        List<DepartmentEntity> departments = departmentService.getDepartments();
+        model.addAttribute("departments", departments);
+
+        return "updateTeamForm";
+    }
+
+    @PostMapping("/equipe/modifier/{id}")
+    public String submitUpdateTeamForm(@PathVariable String id,
+                                           @Valid @ModelAttribute("team") TeamEntity team,
+                                           BindingResult result, Model model,
+                                           RedirectAttributes redirectAttributes) {
+
+        logger.debug("POST /equipe/modifier/" + id);
+
+        if (result.hasErrors()) {
+            logger.info(result.toString());
+
+            // Get employees
+            List<EmployeeEntity> employees =  employeeService.getEmployees();
+            model.addAttribute("employees", employees);
+
+            // Get departments
+            List<DepartmentEntity> departments = departmentService.getDepartments();
+            model.addAttribute("departments", departments);
+
+            // Return form with errors
+            return "updateTeamForm";
+        } else {
+            try {
+
+                // Save team
+                team = teamService.editTeam(team);
+            } catch (Exception e) {
+                logger.error(e.getMessage() + e.getCause());
+                redirectAttributes.addFlashAttribute("message", "Impossible de modifier l'équipe.");
+
+                return "redirect:/equipes";
+            }
+        }
+
+        return "redirect:/equipe/" + team.getId();
     }
 }
