2b3de37ae9655d127d6aa094e5452e609ea7da71
quentin galliou <quentingalliou29@gmail.com>
2019-11-29 08:20:27 +0100

    password reset fix

--- src/main/java/fr/enssat/leave_manager/controller/PasswordResetController.java
+++ src/main/java/fr/enssat/leave_manager/controller/PasswordResetController.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -64,11 +63,9 @@ public class PasswordResetController {
     }
 
     @PostMapping
-    @Transactional
     public String handlePasswordReset(@ModelAttribute("passwordResetForm") @Valid PasswordResetDto form,
                                       BindingResult result,
                                       RedirectAttributes redirectAttributes, @PathVariable String option) {
-
 
         if (result.hasErrors()) {
             redirectAttributes.addFlashAttribute(BindingResult.class.getName() + ".passwordResetForm", result);
